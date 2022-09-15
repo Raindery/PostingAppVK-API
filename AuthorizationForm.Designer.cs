92f415f649bf265e9ee67ebb84777d3f6f537812
Vanilka <vfeodorovm@yandex.com>
2022-09-16 00:30:11 +0400

    Created Authorization form

---
+++ AuthorizationForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace PostingAppVK_API
+{
+    partial class AuthorizationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.authorizationWeb = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // authorizationWeb
+            // 
+            this.authorizationWeb.AllowWebBrowserDrop = false;
+            this.authorizationWeb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.authorizationWeb.IsWebBrowserContextMenuEnabled = false;
+            this.authorizationWeb.Location = new System.Drawing.Point(0, 0);
+            this.authorizationWeb.MinimumSize = new System.Drawing.Size(20, 20);
+            this.authorizationWeb.Name = "authorizationWeb";
+            this.authorizationWeb.ScriptErrorsSuppressed = true;
+            this.authorizationWeb.ScrollBarsEnabled = false;
+            this.authorizationWeb.Size = new System.Drawing.Size(800, 450);
+            this.authorizationWeb.TabIndex = 0;
+            this.authorizationWeb.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // AuthorizationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.authorizationWeb);
+            this.Name = "AuthorizationForm";
+            this.Text = "AuthorizationForm";
+            this.Load += new System.EventHandler(this.AuthorizationForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser authorizationWeb;
+    }
+}
---
+++ AuthorizationForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PostingAppVK_API
+{
+    public partial class AuthorizationForm : Form
+    {
+        public AuthorizationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AuthorizationForm_Load(object sender, EventArgs e)
+        {
+            authorizationWeb.DocumentCompleted += AuthorizationBrowser_DocumentComplited;
+            authorizationWeb.Navigate(VkApi.GetAuthorezationUrl());
+        }
+
+        private void AuthorizationBrowser_DocumentComplited(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+            if(e.Url.ToString().Contains("access_token"))
+            {
+                string argumentsString = e.Url.ToString().Substring(e.Url.ToString().IndexOf('#') + 1);
+                string[] argumentsAndValues = argumentsString.Split('=', '&');
+                int tokenIndex = Array.IndexOf(argumentsAndValues, "access_token");
+                string token = argumentsAndValues[tokenIndex+1];
+
+                UserData.AddUserDataField("token", token);
+                UserData.SaveUserData();
+
+                this.Close();
+            }
+        }
+    }
+}
